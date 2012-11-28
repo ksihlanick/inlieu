02a382de8aa67d2d44e5e29b5e6811d584c8a679
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-11-27 21:17:50 -0500

    initial commit of inlieu app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inlieu::Application.config.secret_token = 'ed8702f9549f064176c28f194fae1eca7bba0829248b3a44f6ab975794292d0793968304dc5bb2bb323c09a86ea0c3669ae1875138de90ff8ec1ace51a47db25'
