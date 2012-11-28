02f290ab234dd67c38f20a013fc79b365267cb55
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-11-28 15:04:41 -0500

    adds user views including show all users, also user sign in and error messages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,6 +16,29 @@ describe "UserPages" do
     it { should have_selector('h1',    text: 'Users#new') }
   end
 
+  describe "signup" do
+    before { visit new_user_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+end
+
   describe "profile page" do
 	let(:user) { FactoryGirl.create(:user) }
 	before { visit user_path(user) }
