a31eade826ae72f5cbc4e3cedcb6bf0f6b973b90
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-12-12 10:09:05 -0500

    changed mail settings again

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -2,7 +2,6 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => 'gmail.com',
   :user_name            => 'kevin.sihlanick@gmail.com',
   :password             => 'GibsonL-1',
   :authentication       => 'plain',
