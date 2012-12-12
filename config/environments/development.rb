814a7e5d995803fe027fa0210fd4f3043e974b1c
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-12-12 10:45:11 -0500

    fixed mail (hopefully)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Inlieu::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -35,4 +35,18 @@ Inlieu::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   config.time_zone = 'UTC'
+
+  config.action_mailer.default_url_options = { host: "localhost:300" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "kevin.sihlanick@gmail.com",
+    password: "Giftofgiving1"
+  }
+
 end
