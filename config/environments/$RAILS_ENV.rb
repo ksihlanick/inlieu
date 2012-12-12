afe2c8c438d50213a1979414fc72030131d06234
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-12-12 10:24:15 -0500

    try again

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -2,7 +2,10 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
+  :domain               => 'https://mighty-gorge-9805.herokuapp.com'
   :user_name            => 'kevin.sihlanick',
   :password             => 'GibsonL-1',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }
+
+  ActionMailer::Base.default_url_options[:host] = "https://mighty-gorge-9805.herokuapp.com"
