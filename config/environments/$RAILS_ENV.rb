d9a043a28be382ded9941821aaa1b7a65cbd33ac
Kevin Sihlanick <kevin.sihlanick@gmail.com>
2012-12-15 11:45:24 -0500

    removed old unused file

--- config/environments/$RAILS_ENV.rb
+++
@@ -1,9 +0,0 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'mighty-gorge-9805.herokuapp.com'
-  :user_name            => 'kevin.sihlanick',
-  :password             => 'GibsonL-1',
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
